37302149dc0affce6697b27e7f63bbee9b73461f
mayo829 <primeguava@gmail.com>
2025-07-21 01:02:11 -0400

    initial

---
+++ RESOURCES/RESOURCES.md
@@ -0,0 +1,20 @@
+# Resources
+
+## Claude
+ - tool use (see 'Best practices for tool definitions'): https://docs.anthropic.com/en/docs/agents-and-tools/tool-use/implement-tool-use
+
+
+## Other
+Command for Creating a new project: npx create-next-app@latest my-new-website --yes --typescript --tailwind --eslint --app --src-dir --import-alias "@/*"
+
+## Reminders for Future Job on what i did
+Paste this stuff into chatgpt to make it better lol
+
+ - efficient researching: find something similar to what im building preferrably open source. Utilize AI to explain architecture and to ask any questions. 
+ - efficient learning: Learn basics through adapting knowledge of c++ and python to new concepts. While developing utilize AI for questions and reference dev docs. Ask questions on forums for things stuck on.
+
+ - Roles: full-stack dev
+ - research: multi-agent systems
+ - services used: langgraph/claude/selenium/supabase/
+ - languages and frameworks: typescript, node.js, 
+ - Others: api routing
---
+++ aicoder/src/code_agent/functions.ts
@@ -0,0 +1,274 @@
+import { inngest } from "./client";
+import { anthropic, createAgent, createTool, createNetwork, type Tool, type Message, createState } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox, lastAssistantTextMessageContent, parseAgentOutput } from "./utils";
+import { stdout } from "process";
+import { z } from "zod";
+import { FRAGMENT_TITLE_PROMPT, PROMPT, RESPONSE_PROMPT } from "@/prompt";
+import { prisma } from "@/lib/db"; 
+import { SANDBOX_TIMEOUT } from "./types";
+
+interface AgentState{
+  summary: string;
+  files:{[path:string]:string};
+}
+// Import the Inngest client and other necessary modules    
+
+
+export const codeAgentFunction = inngest.createFunction(
+  { id: "code-agent" },
+  { event: "code-agent/run" },
+  async ({ event, step}) => {
+    const sandboxId=await step.run("get-sandbox-id", async () => {
+        const sandbox=await Sandbox.create("lamp-nextjs-test2");
+        await sandbox.setTimeout(SANDBOX_TIMEOUT );
+        return sandbox.sandboxId;
+    })
+    const previousMessages= await step.run("get-previous-messages", async () => {
+        const formattedMessages : Message[]=[];
+        const messages= await prisma.message.findMany({
+            where:{
+                projectID: event.data.projectId,
+            },
+            orderBy: {
+                createdAt: "desc",
+            }, 
+            take: 5,
+        });
+        for(const message of messages) {
+            formattedMessages.push({
+                role: message.role==="ASSISTANT" ? "assistant" : "user",
+                content: message.content,
+                type: "text",
+                
+            });
+
+        }
+        return formattedMessages.reverse();
+    });
+    const state =createState<AgentState>(
+        {
+            summary:"",
+            files:{}
+        },
+        {
+            messages: previousMessages,
+        }
+    )
+    const codeAgent = createAgent<AgentState>({
+        name: "code-agent",
+        description:"An expert coding agent",
+        system: PROMPT,
+        model: anthropic({ 
+            model: "claude-3-5-sonnet-20241022",
+            defaultParameters: {
+                temperature: 0.1,
+                max_tokens: 4096
+            }
+        }),
+        tools: [    
+            createTool({
+                name:"terminal",
+                description:"Use the terminal to run commands",
+                parameters: z.object({
+                    command: z.string(),
+            }),
+            handler: async ({command},{step}) => {
+                return await step?.run("terminal", async () => {
+                    const buffers={stdout: "", stderr: ""};
+                    try {
+                        const sandbox = await getSandbox(sandboxId);
+                        const result= await sandbox.commands.run(command,{
+                        onStdout:(data:string) => {
+                            buffers.stdout += data;
+                        },
+                        onStderr:(data:string) => {
+                            buffers.stderr += data;
+                        }
+                    });
+                    return result.stdout
+                }
+                     catch (e) {
+                        console.error(
+                            `Command failled: ${e} \n stdout: ${buffers.stdout} \n stderr: ${buffers.stderr}`,
+                        )
+                        return `Command failled: ${e} \n stdout: ${buffers.stdout} \n stderr: ${buffers.stderr}`;
+                    }
+      });
+    }
+}),
+        createTool({
+            name: "createOrUpdateFiles",
+            description: "Create or update files in the sandbox",
+            parameters: z.object({
+                files: z.array(
+                    z.object({
+                        path:z.string(),
+                        content: z.string(),            
+                    }),
+                )
+            }),
+        
+            handler: async ({files},{step, network}: Tool.Options<AgentState>) => {
+                const newFiles= await step?.run("createOrUpdateFiles", async () => {
+                    try{
+                        const updatedFiles=network.state.data.files || {};
+                        const sandbox= await getSandbox(sandboxId);
+                        for (const file of files) {
+                            await sandbox.files.write(file.path, file.content);
+                            updatedFiles[file.path] = file.content;
+                        }
+
+                        return updatedFiles;
+
+                    } catch (e) {
+                        return "Error:"+e;
+                        
+                    }
+            });
+            if(typeof newFiles === "object") {
+                network.state.data.files = newFiles;
+   
+            }
+        
+    
+        }
+    }),
+        createTool({
+            name: "readFiles",
+            description: "Read files from the sandbox",
+            parameters: z.object({
+                files:z.array(z.string()),
+            }),
+            handler: async ({files},{step}) => {
+                return await step?.run("readFiles", async () => {
+                    try{
+                        const sandbox = await getSandbox(sandboxId);
+                        const contents = [];
+                        for (const file of files) {
+                            const content = await sandbox.files.read(file);
+                            contents.push({path: files,content});
+                        }
+                        return JSON.stringify(contents);
+                    } catch (e) {
+                        return "Error:"+e;
+                    }
+                    
+                });
+            }
+        })
+],
+lifecycle:{
+  onResponse: async({result, network})=>{
+    const lastAssistantMessageText =lastAssistantTextMessageContent(result);     
+if(lastAssistantMessageText && network) {
+    if(lastAssistantMessageText.includes("task_summary")){
+        network.state.data.summary = lastAssistantMessageText;
+    }
+      
+    }           
+    return result;
+},
+
+
+    
+  
+}
+  });
+  const network=createNetwork<AgentState>({
+    name:"coding-agent-network",
+    agents: [codeAgent],
+    maxIter:15,
+    defaultState: state,
+    router:async({network})=>{
+        const summary =network.state.data.summary;
+        if(summary) {
+            return;
+        }
+        return codeAgent;
+    }
+    });
+
+      console.log("Calling LLM with:", event.data.value);
+
+      const result= await network.run(event.data.value,{state: state});
+
+      const fragmentTitleGenerator=createAgent({
+        name: "fragment-title-generator",
+        description:"A fragment title generator",
+        system: FRAGMENT_TITLE_PROMPT,
+        model: anthropic({ 
+            model: "claude-3-5-sonnet-20241022",
+            defaultParameters: {
+                temperature: 0.1,
+                max_tokens: 4096
+            }
+        }),
+      })
+
+      const responseGenerator=createAgent({
+        name: "response-generator",
+        description:"A respnse generator",
+        system:  RESPONSE_PROMPT,
+        model: anthropic({ 
+            model: "claude-3-5-sonnet-20241022",
+            defaultParameters: {
+                temperature: 0.1,
+                max_tokens: 4096
+            }
+        }),
+      })
+
+      const {output:fragmentTitleOutput}= await fragmentTitleGenerator.run(result.state.data.summary);
+      const {output:responseOutput}= await responseGenerator.run(result.state.data.summary);
+     
+
+      
+
+      const isError=
+      !result.state.data.summary || 
+      Object.keys(result.state.data.files ||{}).length===0
+
+      const sandboxUrl= await step.run("get-sandbox-url", async () => {
+        const sandbox = await getSandbox(sandboxId);
+        const host= sandbox.getHost(3000);
+        return `https://${host}`;
+      });
+      await step.run("save-result", async()=>{
+        if(isError){
+          return await prisma.message.create({
+            data:{
+                projectID: event.data.projectId,
+              content: "Something went wrong, please try again.",
+              role : "ASSISTANT",
+              type:"ERROR",
+            },
+          })
+        }
+        return await prisma.message.create({
+          data:{
+            projectID: event.data.projectId,
+            content: parseAgentOutput(responseOutput),
+            role:"ASSISTANT",
+            type:"RESULT",
+            fragment:{
+              create:{
+                sandboxUrl: sandboxUrl,
+                title: parseAgentOutput(fragmentTitleOutput),
+                files: result.state.data.files,
+
+              }
+            }
+          },
+        })
+      })
+      
+
+      
+
+    return {
+        url: sandboxUrl ,title:"Fragment",
+         files: result.state.data.files ,
+          summary: result.state.data.summary };
+  },
+);
